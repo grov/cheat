9fd282859c646867debb166a0a1aa381ad8fa7db
Johnny John Johnnson <yoann.audon@gmail.com>
2017-05-12 18:26:18 +0200

    Update full-install.sh

--- full-install.sh
+++ full-install.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
 # Installe automatiquement les prerequis ... wouahou !
+
+# On installe pip
 apt-get install python-pip
+
+# On upgrade pip
 pip install --upgrade pip
+
+# On install les prérequis pour cheat
 pip install docopt pygments appdirs
+
+# On clone le dépôt cheat
 git clone https://github.com/grov/cheat.git
+
+# On rend executable le setup
 chmod +x setup.sh
+
+# installation
 python setup.py install
 
 echo "Installation complete ... test 'cheat nmap' command"
